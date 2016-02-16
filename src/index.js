140b6e3b8b4bbf769cbc059bfdd83417ab1adf16
Tyler Johnson <tyler@vintyge.com>
2016-02-15 23:45:04 -0700

    feat: removed plain object and array tracking

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-import isPlainObject  from "is-plain-object";
-import { trackProperty, trackObject } from "./object";
-import { trackArray } from "./array";
+import { trackProperty } from "./property";
 import { Map } from "./map";
 import { List } from "./list";
 import { Variable } from "./var";
 
-export { List, Map, Variable, trackProperty, trackObject, trackArray };
-
-export function track(obj, replacer) {
-	function replace(k, v) {
-		var nval;
-		if (typeof replacer === "function") nval = replacer.apply(this, arguments);
-		if (typeof nval === "undefined" && typeof v !== "undefined") nval = track(v);
-		return nval;
-	}
-
-	if (Array.isArray(obj)) return trackArray(obj, replace);
-	if (isPlainObject(obj)) return trackObject(obj, replace);
-	return obj;
-}
+export { List, Map, Variable, trackProperty };
